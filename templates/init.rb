d7981f2dd6977f0b986560288d1e5857e6a5b4fa
nicwilliams <nicwilliams@6082755c-0520-0410-bda1-dcdef166d76e>
2008-01-29 10:02:25 +0000

    Loads vendored gems lib path into LOAD_PATH first, then runs any gem init.rb files [thx Chris Kampmeier]
    git-svn-id: svn+ssh://rubyforge.org/var/svn/gemsonrails/trunk@33 6082755c-0520-0410-bda1-dcdef166d76e

--- templates/init.rb
+++ templates/init.rb
@@ -1,8 +1,13 @@
 gems = Dir[File.join(RAILS_ROOT, "vendor/gems/*")]
 if gems.any?
-  gems.sort.each do |dir|
+  # Prepend load paths first so that gems can depend on eachother
+  gems.each do |dir|
     lib = File.join(dir, 'lib')
     $LOAD_PATH.unshift(lib) if File.directory?(lib)
+  end
+  
+  # Require each gem
+  gems.each do |dir|
     init_rb = File.join(dir, 'init.rb')
     require init_rb if File.file?(init_rb)
   end
